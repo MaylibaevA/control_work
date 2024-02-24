f2dddc2810fbbd58cea5806c54a80a9a07b7b1b5
Aman Maylibaev <a.maylibaev@mail.ru>
2024-02-25 03:40:44 +0600

    Инициализация массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите желаемое количество элементов Вашего массива:");
+Console.WriteLine();
+int indexAmount = Convert.ToInt32(Console.ReadLine());
+string [] strings = new string [indexAmount];
+int index = 0;
+while(indexAmount > index)
+{
+    Console.Write("Введите свое значение:");
+    Console.WriteLine();
+    strings[index] = Console.ReadLine();
+    index++;
+}
+
